938b73efb9b6afa0701b0ef8235a46ef00940ee8
Patrick Bien <pbien1330@gmail.com>
2022-07-06 19:10:52 +0200

    message changed

---
+++ raceresults/raceresults/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  raceresults
+//
+//  Created by Patrick Bien on 06.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, race!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ raceresults/raceresults/raceresultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  raceresultsApp.swift
+//  raceresults
+//
+//  Created by Patrick Bien on 06.07.22.
+//
+
+import SwiftUI
+
+@main
+struct raceresultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
